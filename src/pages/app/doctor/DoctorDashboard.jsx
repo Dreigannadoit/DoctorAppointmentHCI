3133fdaef5c41250e796488fab31ff852d91a94a
Drei Abmab <dreiabmab@gmail.com>
2025-05-20 10:09:30 +0800

    Added doctor link

--- src/pages/app/doctor/DoctorDashboard.jsx
+++ src/pages/app/doctor/DoctorDashboard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const DoctorDashboard = () => {
   return (
-    <div>DoctorDashboard</div>
+    <div>
+      <a href="https://www.figma.com/design/Nrd0x4itHuyh61OL4lTNuA/Untitled?node-id=1-607&t=R7rTN8AYOaxqTzcL-0">Doctor Design</a>
+    </div>
   )
 }
 
